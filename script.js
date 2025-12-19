e120b35b1b1c19d46f9e7cc453d1375910850446
PRANEETH <145996746+Praneeth1136@users.noreply.github.com>
2025-12-19 10:52:42 +0530

    Laying the Foundation

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-const heading = React.createElement('h1', { id: 'heading' }, 'Hello World from React');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-console.log(heading); //Object
-const root = ReactDOM.createRoot(document.querySelector('.root'));
+// const heading = React.createElement('h1', { id: 'heading' }, 'Hello from react');
 
-root.render(heading);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const JSXheading = () => <h1 id="heading">Hello from JSXheading</h1>;
+
+//ReactComponent
+
+//Component Composition
+
+const number = 2000;
+
+const FunctionalComponent = () => {
+    return (
+        <div className="container">
+            {number}
+            <h2>{number}</h2>
+            {JSXheading()}
+            <JSXheading />
+            <JSXheading></JSXheading>
+            <h1 className="heading">Hello World</h1>
+        </div>
+    );
+};
+
+// const functionalComponent2 = () => (
+//     <h1 className="heading">Hello from FC2</h1>
+// );
+
+// const functionaLComponent3 = () => <h1>Hello World</h1>
+
+// root.render(heading);
+// root.render(JSXheading);
+
+root.render(<FunctionalComponent />);
